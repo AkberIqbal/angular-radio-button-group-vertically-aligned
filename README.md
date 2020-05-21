ea6ffd475ff4a14ea5a8df1856477f3854107289
AkberIqbal <akber.iqbal@gmail.com>
2020-05-21 14:55:48 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-bzybcw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-bzybcw)
---
+++ app/radio-ng-model-example.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Radios with ngModel
+ */
+@Component({
+  selector: 'radio-ng-model-example',
+  templateUrl: 'radio-ng-model-example.html',
+  styleUrls: ['radio-ng-model-example.css'],
+})
+export class RadioNgModelExample {
+}
+
+
+/**  Copyright 2019 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
